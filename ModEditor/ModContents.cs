a80d6ed65b7061d69c461a4c135321832aa28cef
FrostHand <dv_frost@mail.ru>
2013-05-20 19:03:08 +0400

    string editor is mostly ok

--- ModEditor/ModContents.cs
+++ ModEditor/ModContents.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework.Graphics;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -22,7 +24,7 @@ namespace ModEditor
         {
             Empty,
             Unloaded,
-            Loaded
+            Loaded 
         }
 
         public State state = State.Empty;
@@ -39,14 +41,22 @@ namespace ModEditor
             Reset();
 
             AddController(new ModContents.ArtifactsSpec());
+            AddController(new ModContents.EventsGroup());
+            AddController(new ModContents.HullsGroup());
             AddController(new ModContents.BuildingSpec());
-            AddController(new ModContents.ModuleSpec());
-            AddController(new ModContents.WeaponGroup());
+            AddController(new ModContents.ModuleSpec());            
             AddController(new ModContents.TechSpec());
+            AddController(new ModContents.TexturesGroup());
             AddController(new ModContents.TroopSpec());
+            AddController(new ModContents.WeaponGroup());
+
+            AddController(new ModContents.StringsGroup());
             /*
-            controllers.Add(new Workspace.TexturesSpec());
-            controllers.Add(new Workspace.ModelsSpec());*/
+             	Races
+	            Encounters 
+             * Strings
+             * ToolTips
+             */
         }
 
         public bool Loaded()
@@ -272,6 +282,69 @@ namespace ModEditor
             public abstract void UpdateItems();
         }
 
+        public class StringsGroup : Controller
+        {
+            ItemBase strings;
+            DataTable dt = new DataTable();
+
+            public StringsGroup()
+            {
+                strings = new ItemBase(null, this);
+                strings.controller = this;
+                strings.name = "Strings";
+
+                dt.Columns.Add(new DataColumn("token", typeof(int)));
+                dt.Columns.Add(new DataColumn("text", typeof(string)));
+                dt.Columns.Add(new DataColumn("refs", typeof(int)));                  
+            }
+
+            public override void ClearCache()
+            { 
+
+            }
+
+            public override Control GenerateControl(ItemBase item)
+            {
+                DataGridView result = new DataGridView();
+                result.DataSource = dt;
+                result.Dock = DockStyle.Fill;
+                result.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.AllCells;
+                //result.Columns["text"].DefaultCellStyle.WrapMode = DataGridViewTriState.True;
+                return result;
+            }
+
+            public override void ObtainModData(bool isBase)
+            {
+                foreach (var item in Ship_Game.Localizer.LocalizerDict)
+                {
+                    object[] row = { item.Key, item.Value, 0 };
+                    dt.Rows.Add(row);
+                }
+            }
+
+            public override void PopulateModOverview(TreeNodeCollection root)
+            {
+                TreeNode groupNode = null;
+                string folder = "Strings";
+                if (root.ContainsKey(folder))
+                    groupNode = root[folder];
+                else
+                {
+                    groupNode = new TreeNode(folder);
+                    root.Add(groupNode);
+                }
+                groupNode.Tag = strings;
+                strings.node = groupNode;
+            }
+
+            public override void Save(string dir)
+            {
+            }
+
+            public override void UpdateItems()
+            { 
+            }
+        }
         public abstract class ControllerSpec<Target> : Controller
         {            
             // Global items cache            
@@ -412,6 +485,79 @@ namespace ModEditor
             {
                 return "Weapons";
             }            
+        }
+
+        public class TexturesGroup : ControllerSpec<Texture2D>
+        {
+            public override XmlSerializer GetSerializer()
+            {
+                return Ship_Game.ResourceManager.weapon_serializer;
+            }
+
+            public override Dictionary<string, Texture2D> GetStorage()
+            {
+                return Ship_Game.ResourceManager.TextureDict;
+            }
+
+            public override string GetGroupFolder()
+            {
+                return "Textures";
+            }
+        }
+
+        public class EventsGroup : ControllerSpec<Ship_Game.ExplorationEvent>
+        {
+            public override XmlSerializer GetSerializer()
+            {
+                return Ship_Game.ResourceManager.weapon_serializer;
+            }
+
+            public override Dictionary<string, Ship_Game.ExplorationEvent> GetStorage()
+            {
+                return Ship_Game.ResourceManager.EventsDict;
+            }
+
+            public override string GetGroupFolder()
+            {
+                return "Exploration Events";
+            }
+        }
+
+        /*
+        public class RacesGroup : ControllerSpec<Ship_Game.ShipData>
+        {
+            public override XmlSerializer GetSerializer()
+            {
+                return Ship_Game.ResourceManager.weapon_serializer;
+            }
+
+            public override Dictionary<string, Ship_Game.ShipData> GetStorage()
+            {
+                return Ship_Game.ResourceManager.HullsDict;
+            }
+
+            public override string GetGroupFolder()
+            {
+                return "Races";
+            }
+        }*/
+
+        public class HullsGroup : ControllerSpec<Ship_Game.ShipData>
+        {
+            public override XmlSerializer GetSerializer()
+            {
+                return Ship_Game.ResourceManager.weapon_serializer;
+            }
+
+            public override Dictionary<string, Ship_Game.ShipData> GetStorage()
+            {
+                return Ship_Game.ResourceManager.HullsDict;
+            }
+
+            public override string GetGroupFolder()
+            {
+                return "Hulls";
+            }
         }
 
         public class TechSpec : ControllerSpec<Ship_Game.Technology>
